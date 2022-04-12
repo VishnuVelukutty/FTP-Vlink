293abd2e1ba140123903f8c5484af1cb69c83afe
VishnuVelukutty <vishnuvelukutty26@gmail.com>
2022-04-12 19:55:53 +0530

    2

---
+++ assignment-2/prime.java
@@ -0,0 +1,12 @@
+public class prime{
+    public static void main(String args[]) {
+
+        int x=10;
+        for(int y=1;y<=x;y++){
+            if(y%2==0){
+                System.out.println("Even Number"+y);
+            }
+        }
+        
+    }
+}
